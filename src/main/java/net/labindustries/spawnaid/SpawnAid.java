fd93c95a6d355cb0685e0c858f9ae733b67094cd
LukasKlab <lukas.bendiksen@gmail.com>
2022-07-21 11:57:27 +0200

    Forgot some config

--- src/main/java/net/labindustries/spawnaid/SpawnAid.java
+++ src/main/java/net/labindustries/spawnaid/SpawnAid.java
@@ -7,9 +7,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpawnAid extends JavaPlugin {
 
+
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
 
         getCommand("setspawn").setExecutor(new SetSpawnCommand(this));
         getCommand("spawn").setExecutor(new SpawnCommand(this));
