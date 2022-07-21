1941c7e03ecd5aa4cdbecfa842e88bcc232c3928
LukasKlab <lukas.bendiksen@gmail.com>
2022-07-21 11:35:47 +0200

    Initial Commit

---
+++ src/main/java/net/labindustries/spawnaid/SpawnAid.java
@@ -0,0 +1,17 @@
+package net.labindustries.spawnaid;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnAid extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
